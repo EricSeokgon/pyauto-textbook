7cdb9276a01980f1c6fc5ff4950a8b3419dd1282
hadesyi <leesk55@gmail.com>
2022-05-27 16:43:39 +0900

    날짜 시간 구하기

--- ch1/dt_now.py
+++ ch1/dt_now.py
@@ -1,7 +1,6 @@
 # 날짜 시간 관련 처리를 하는 모듈
-from datetime import datetime as dt
+import datetime
 
 # 현재 시각 구하기
-t = dt.now()
-fmt = t.strftime('%Y년%m월%d일 %H시%M분%S초')
-print(fmt)
+t = datetime.datetime.now()
+print(t)
