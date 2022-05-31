3e01f5cd52dcf9136242b53ef7ed2ec7e15d10dd
hadesyi <leesk55@gmail.com>
2022-05-31 14:46:27 +0900

    남은 날짜 계산하기

--- ch1/dt_delta2.py
+++ ch1/dt_delta2.py
@@ -4,7 +4,7 @@ sleep_t = datetime(2023, 1, 1, 22, 0, 0)
 wakeup_t = datetime(2023, 1, 2, 8, 30, 0)
 
 delta = wakeup_t - sleep_t
-sec = delta.secons
+sec = delta.seconds
 hours = sec / (60 * 60)
 
 print('수면 시간은'+str(hours)+'시간입니다.')
