99c327d07f7174546df148fe14ea1fd96f159ac0
hadesyi <leesk55@gmail.com>
2022-05-31 15:50:48 +0900

    생년 나이 대응표 만들기

---
+++ ch2/write2_agelist.py
@@ -0,0 +1,45 @@
+import openpyxl as excel
+import datetime
+
+# 새 워크북 생성
+book = excel.Workbook()
+sheet = book.active
+
+# 올해 연도 구하기
+thisyear = datetime.datetime.now().year
+
+# 1행에 헤더 설정
+sheet["A1"] = "출생 연도"
+sheet["B1"] = "세는나이"
+sheet["C1"] = "만 나이는 (생일 후)"
+sheet["D1"] = "만 나이는 (생일 전)"
+
+# 셀의 너비 조정
+sheet.column_dimensions["C"].width = 15
+sheet.column_dimensions["D"].width = 15
+
+# 생년 나이 연속 데이터 채우기
+for i in range(80):
+    # 설정할 값을 계산
+    birth_year = thisyear
+    korean_age = thisyear - birth_year + 1  # 세는 나이는 1살부터 시작
+    man_age = {'after_bday': korean_age - 1, 'before_bday': korean_age - 2}
+
+    # 셀을 읽어 값을 설정하기
+    year_cell = sheet.cell(i + 2, 1)
+    year_cell.value = str(birth_year) + "년생"
+
+    age_cell = sheet.cell(i + 2, 2)
+    age_cell.value = str(korean_age) + " 세"
+
+    age_cell = sheet.cell(i + 2, 3)
+    age_cell.value = "만 " + str(man_age['after_bday']) + " 세"
+
+    age_cell = sheet.cell(i + 2, 4)
+    age_cell.value = "만 " + str(man_age['before_bday']) + " 세"
+
+# 예외 경우 처리
+sheet["D2"] = "-"
+
+# 파일 저장
+book.save("output/write2_agelist.xlsx")
