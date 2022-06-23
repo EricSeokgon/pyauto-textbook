2a3acdced84306bf532eab0da961097e3eb37d8a
hadesyi <leesk55@gmail.com>
2022-06-23 10:47:05 +0900

    성명 시트의 열을 합치거나 분할하기 -성명 필드 분할히기

---
+++ ch3/name_split.py
@@ -0,0 +1,27 @@
+import openpyxl as excel
+
+# 입출력 파일 지정
+in_file = "./input/name1.xlsx"
+out_file = "./output/name_split.xlsx"
+
+# 입력 문서를 열고 시트 가져오기
+book = excel.load_workbook(in_file)
+sheet = book.worksheets[0]
+
+# 신규 문서를 생성해 시트 가져오기
+out_book = excel.Workbook()
+out_sheet = out_book.active
+
+# 시트의 행 읽기
+for row in sheet.iter_rows():
+    # 앞뒤 공백이 제거된 성명 가져오기
+    name = row[0].value.strip()
+    # 성과 이름 나누기
+    if ' ' in name:
+        sung, myung = name.split(' ')
+    else:
+        sung, myung = name[0], name[1:]
+    # 신규 시트에 추가하기
+    out_sheet.append([sung, myung])
+# 결과를 저장
+out_book.save(out_file)
